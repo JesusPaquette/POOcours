82f934d71434214559f502c918532d8a1367cece
JesusPaquette <66388884+JesusPaquette@users.noreply.github.com>
2021-02-03 14:37:34 -0500

    Prenière version

---
+++ premierCsharp/Program.cs
@@ -0,0 +1,147 @@
+﻿// Mon premier programme C#
+using System;
+
+
+class Program
+{​​​​​​
+    static void Main()
+    {​​​​​​
+        TestDeValeurs();
+
+        Console.WriteLine("Appuyer sur une touche pour continuer");
+        Console.ReadKey();
+    }​​​​​​
+ 
+    static void TestDeBase()
+    {​​​​​​
+        // WrtieLine inclue le changement de ligne
+        Console.WriteLine("Bonjour tout le monde");
+
+        Console.WriteLine("Appuyer sur une touche pour continuer");
+        Console.ReadKey();
+
+        Console.Clear();
+        Console.WriteLine("Bienvenue au cours de\nProgrammation Orientée Objet");
+
+        //Types de bases
+        int entier = 12;
+        double nombreReel = 34.56;
+        char caractere = 'C';
+        bool booleen = true;
+        //Types complexes
+        string chaineDeCaracteres = "Bonjour";
+        //Constantes
+        const double Pi = 3.1416;
+
+        // C + W + TAB + TAB = Console.WriteLine
+        Console.WriteLine("Entier: " + entier + " réel: " + nombreReel + ", caractère: " + caractere
+        + ", booléen: " + booleen + ", chaine: " + chaineDeCaracteres);
+
+        Console.WriteLine("Entier: {​​​​​​0}​​​​​​, réel: {​​​​​​1}​​​​​​, chaineDeCaracteres: {​​​​​​2}​​​​​​, l'entier est toujours: {​​​​​​0}​​​​​​",
+            /*position 0*/entier, /*1*/nombreReel, /*2*/chaineDeCaracteres);
+
+        //Lecture de la console
+        Console.Write("Entrez du texte: ");
+        string lecture = Console.ReadLine();
+        Console.WriteLine("Texte: \"" + lecture + "\"");
+
+        ;
+    }​​​​​​
+ 
+    //Lit une chaîne de caractère et retourne sa conversion en entier
+    static int ObtenirEntier(int min, int max)
+    {​​​​​​
+        Console.WriteLine("Entrez un entier entre " + min + " et " + max + ": ");
+        string ligne = Console.ReadLine();
+
+        //On ne valide pas les erreurs, le programme va planter si l'utilisateur ne donne pas un entier
+        return Convert.ToInt32(ligne);
+    }​​​​​​
+ 
+    static void TestDeControles()
+    {​​​​​​
+        const int Min = 1;
+        const int Max = 10;
+
+        for (int i = 0; i < 2; ++i)
+        {​​​​​​
+            Console.WriteLine("Tentative " + i + 1 + "/2");
+
+            while (true)
+            {​​​​​​
+                int entier = ObtenirEntier(Min, Max);
+                if (entier >= Min && entier <= Max)
+                {​​​​​​
+                    switch (entier)
+                    {​​​​​​
+                       case 1:
+                                Console.WriteLine("Un");
+                        break;
+                        case 2: 
+                                Console.WriteLine("Deux");
+                        break;
+                        case 3:
+                                Console.WriteLine("Trois");
+                        break;
+                        
+                                Console.WriteLine("Quatre");
+                        break;
+                        default:
+                                Console.WriteLine("Autre valeur");
+                        break;
+                    }
+                }​​​​​​
+                else
+            {​​​​​​
+                    Console.WriteLine("Valeur incorrect, esseyez de nouveau");
+            }​​​​​​
+            
+        }​​​​​​
+ 
+        Console.Write("entrez un chiffre textuellement: ");
+        string chiffreEnTexte = Console.ReadLine();
+ 
+        switch (chiffreEnTexte)
+        {​​​​​​
+ 
+            // Un case doit absolumentavoir un break si une instruction est executée
+            case "un":
+            case "Un":
+                Console.WriteLine("Le chiffre est 1");
+                break;
+            case "deux":
+            case "Deux":
+                Console.WriteLine("Le chiffre est 1");
+                break;
+            case "trois":
+            case "Trois":
+                Console.WriteLine("Le chiffre est 1");
+                break;
+            default:
+                Console.WriteLine("Chiffre inconnu");
+                break;
+        }​​​​​​
+    }​​​​​​
+}
+ 
+ static void TestDeValeurs()
+{​​​​​​
+    int nombre1 = 10;
+    int nombre2 = 20;
+    int nombre3 = 30;
+
+    //ObtenirValeurs(nombre1, nombre2, nombre3);
+    Console.WriteLine("Valeurs d'origine: " + nombre1 + ", " + nombre2 + ", " + nombre3);
+
+    ModifierValeurs(ref nombre1, ref nombre2, ref nombre3);
+    Console.WriteLine("Valeurs modifiées: " + nombre1 + ", " + nombre2 + ", " + nombre3);
+}​​​​​​
+ 
+
+    static void ModifierValeurs(ref int valeur1, ref int valeur2, ref int valeur3)
+{​​​​​​
+        valeur1 = valeur2 * 5;
+    valeur2 += 100;
+    valeur3 = (valeur2 + 3) % 7;
+
+}​​​
---
+++ tableau et nouvelle matiere/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace tableau_et_nouvelle_matiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            testChaineDeCaracteres();
+        }
+        static void tableau()
+        {
+            int[] tab = { 1, 2, 3, 4, 5, 6, 7 };
+            for(int x = 0; x < 7; x++)
+            {
+                Console.WriteLine(tab[x]);
+            }
+            for (int x = 0; x < 7; x++)
+            {
+                Console.Write(tab[x] + ", ");
+            }
+            Console.WriteLine();//changer de ligne
+            //for each 
+            foreach (int item in tab)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();//changer de ligne
+            //tableau de string
+            string[] nomsMois = { "Janvier", "Fevrier", "mars","mai" };
+            Console.WriteLine("le 2e mosi est : " + nomsMois[1]);
+            nomsMois[3] = "avril";
+            foreach (string item in nomsMois)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+        static void testChaineDeCaracteres()
+        {
+            string maChaine = "bonjour";
+            // lengnt est disponnible pour les strings
+            Console.WriteLine("Longueur de la chaine: " + maChaine.Length + "Le premier caractere est: " +
+                maChaine[0] + ", LE DERNier est : " + maChaine[maChaine.Length - 1]);
+            foreach (char item in maChaine)
+            {
+                Console.WriteLine(item);
+            }
+            //maChaine[1] = 'a'; impossible
+            maChaine = "ceci est ";//peut changer la valeur au complet cest tout
+            maChaine += "un test ";//peut ajouter comme sa "ceci est un test"
+            maChaine += 't';// += fonctionne mais va toujours ajouter juste a la fin
+
+            string[] mots = maChaine.Split(' ');
+            foreach (string item in mots)
+            {
+                Console.WriteLine(item);
+            }
+            maChaine = "               Une chaine de caracteres avec des espace avant et apres.             ";
+            Console.WriteLine("chaine original: \"{0}\"", maChaine);
+            //trimstart
+            //trimEnd
+            //enleve espace debut ou fin
+            maChaine.Trim();
+            Console.WriteLine("chaine sans espace: \"{0}\"", maChaine);
+            maChaine = maChaine.Trim();
+            Console.WriteLine("chaine sans espace(pris2): \"{0}\"", maChaine);
+
+            string aChercher = "une";
+            if (maChaine.Contains(aChercher))
+            {
+                Console.WriteLine("contien :" + aChercher);
+                if (maChaine.StartsWith(aChercher))
+                {
+                    Console.WriteLine("la chaine debut par :" + aChercher);
+                }
+                else
+                {
+                    Console.WriteLine("la chaine ne debut pas par :" + aChercher);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ne contient pas :" + aChercher);
+            }
+        }
+        
+    }
+}
